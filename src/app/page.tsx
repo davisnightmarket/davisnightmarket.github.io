d0b56f885fd82be750361b99b901a248d1eb7c3f
Felix Moses <felix@tacocat.com>
2024-05-09 23:30:39 -0700

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,35 @@
+import { Html } from "next/document";
+import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
     return (
-        <div>
-            <div id="header">
-                <span>[image]</span>
-                <a>{"Donate".toUpperCase()}</a>
-                <a>{"Give Food".toUpperCase()}</a>
-                <a>{"Get Involved".toUpperCase()}</a>
+        <html>
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Poetsen+One&display=swap" rel="stylesheet" />
+            </Head>
+            <body>
+
+            <Image
+                src="/name.png"
+                width={100}
+                height={75}
+                alt="logo"
+            />
+            <div style={{
+                margin: "auto",
+                width: 600,
+                font: 20,
+                paddingTop: 20,
+                fontFamily: '"Poetsen One", sans-serif',
+                fontWeight: 400,
+                fontStyle: 'normal',
+            }}>
+                The Davis Night Market
             </div>
-        </div>
+            </body>
+        </html>
     );
 }
